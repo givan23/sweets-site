cbbd0a3bde7805578e4bd13c2a7f27af7cfed458
givan23 <ivan.trabucco@yahoo.it>
2021-07-04 14:06:39 +0200

    add new project structure

--- README.md
+++
@@ -1,1 +0,0 @@
-ciaone, come stai ?
--- prova/modifica.tsx
+++
@@ -1,7 +0,0 @@
-const saluto = () => {
-    const risposta = "io bene perchè cucini tu!"
-    return "ciaone, come stai?" + risposta;
-};
-
-// new comment
-// un comentario nuevo
