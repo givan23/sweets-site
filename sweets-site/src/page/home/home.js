4746d55bdfbfecd77eb124578655cc2e57fd6df3
givan23 <ivan.trabucco@yahoo.it>
2021-07-08 19:06:01 +0200

    add card list in home page

--- sweets-site/src/page/home/home.js
+++ sweets-site/src/page/home/home.js
@@ -1,12 +1,15 @@
-import React from "react"
-import "./home.css"
-import { CardComponent } from "../../components/cards/card"
+import React from "react";
+import "./home.css";
+import { CardComponent } from "../../components/cards/Card";
 
 export const HomeComponent = () => {
-    return <div  className = "container" > <CardComponent/> <CardComponent/> <CardComponent/> <CardComponent/> 
-    <CardComponent/> <CardComponent/><CardComponent/> <CardComponent/>
-     <CardComponent/><CardComponent/> <CardComponent/> <CardComponent/> 
-     <CardComponent/> <CardComponent/> <CardComponent/> 
-         </div>
-}
- 
+  const cardList = [{ like: 10 }, { like: 50 }, { like: 25 }, { like: 80 }];
+
+  return (
+    <div className="container">
+      {cardList.map((card) => {
+        return <CardComponent like={card?.like} />;
+      })}
+    </div>
+  );
+};
